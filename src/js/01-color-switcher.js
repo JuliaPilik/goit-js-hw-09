863ad24e935d83bde71ae960934cc30165dde7fd
Julia Meshchaninova <j.meschaninova@gmail.com>
2022-12-10 19:38:00 +0200

    update files

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const timer = {
+//     start() {
+//         const startTime = Date.now();
+    
+
+//         setInterval(() => {
+//             console.log('sdfsd');
+//         }, 1000);
+
+//     },
+// }
+
+// timer.start();
+console.log("sdfdfdgdgfhf");
+alert('dscvdfvfd');
